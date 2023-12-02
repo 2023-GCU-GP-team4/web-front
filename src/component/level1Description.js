c8c32918de8213b1df66af9c80a6db9ad1bdd67d
jy <hjiyeon2282@gmail.com>
2023-12-02 20:47:49 +0900

    level 완료

--- src/component/level1Description.js
+++ src/component/level1Description.js
@@ -4,8 +4,22 @@ import "./levelDescriptionBlockEvent.css";
 
 import {Link} from 'react-router-dom';
 import React, { Component } from 'react';
+
+// level마다 변경, level(page) 이동
 import { Level3 } from './level3Description';
 import { Level2 } from './level2Description';
+
+// 사진
+// 고정 
+import arrowL_mouseleave from "../img/level_arrow_left_mouseleave.png";
+import arrowL_mouseover from "../img/level_arrow_left_mouseover.png";
+import arrowR_mouseleave from "../img/level_arrow_right_mouseleave.png";
+import arrowR_mouseover from "../img/level_arrow_right_mouseover.png";
+import check from "../img/level_checkbox.png";
+import logo from "../img/BottomLogo.png";
+// level마다 변경
+import pic_mouseleave from "../img/level1_mouseleave.png";
+import pic_mouseover from "../img/level1_mouseover.png";
 
 export default function Level1()
 {
@@ -15,32 +29,32 @@ export default function Level1()
             <div className="contents">
                 <div className="middle">
                     <Link to="/level/3" className="arrow">
-                        <img src="https://drive.google.com/uc?id=1trFjEz_Klfh2ie2nrH_i-JuwDTPEbplm" alt="돌아가기" className="arrow_mouseleave"></img>
-                        <img src="https://drive.google.com/uc?id=1ErT6QYYejruAkI7Y3T1IZbqlo_EnGlQq" alt="돌아가기_전환" className="arrow_mouseover"></img>
+                        <img src={arrowL_mouseleave} alt="돌아가기" className="arrow_mouseleave"></img>
+                        <img src={arrowL_mouseover} alt="돌아가기_전환" className="arrow_mouseover"></img>
                     </Link>
                     <div className="outline">
                         <div className="pic">
-                            <img src="https://drive.google.com/uc?id=1EuGxelOxk73i6ZCNVm3LJsJdjly6pQ7l" alt="이미지" className="pic_mouseleave"></img>
-                            <img src="https://drive.google.com/uc?id=1TlhdIInvKOOuGd3jX1i4vYYMhEDf-DYJ" alt="이미지" className="pic_mouseover"></img>
+                            <img src={pic_mouseleave} alt="이미지" className="pic_mouseleave"></img>
+                            <img src={pic_mouseover} alt="이미지" className="pic_mouseover"></img>
                         </div>
                         <div className="explanation">
                             <div className="set">
-                                <img src="https://drive.google.com/uc?id=1yw9r_8uh7-dr7XVN7n7iTumZCrjDc-SA" alt="체크박스" className="checkbox"></img>
+                                <img src={check} alt="체크박스" className="checkbox"></img>
                                 <h2> No audience </h2>
                             </div>
                             <div className="set">
-                                <img src="https://drive.google.com/uc?id=1yw9r_8uh7-dr7XVN7n7iTumZCrjDc-SA" alt="체크박스" className="checkbox"></img>
+                                <img src={check} alt="체크박스" className="checkbox"></img>
                                 <h2> No time limit </h2>
                             </div>
                         </div>
                     </div>
                     <Link to="/level/2" className="arrow">
-                        <img src="https://drive.google.com/uc?id=19QGRXq-A1BGzo6z8CVch9sWr72FgPssN" alt="넘어가기" className="arrow_mouseleave"></img>
-                        <img src="https://drive.google.com/uc?id=1LgGkB-EVm35T4tZTeyoPcMC6d8rysOCu" alt="넘어가기_전환" className="arrow_mouseover"></img>
+                        <img src={arrowR_mouseleave} alt="넘어가기" className="arrow_mouseleave"></img>
+                        <img src={arrowR_mouseover} alt="넘어가기_전환" className="arrow_mouseover"></img>
                     </Link>
                 </div>
 
-                <img src="https://drive.google.com/uc?id=1Cgna9D1Zvnz-I3B_BVsjIUlNrMXPUbX5" alt="로고" className="logo"></img>
+                <img src={logo} alt="로고" className="logo"></img>
             </div>
         </div>
     )
