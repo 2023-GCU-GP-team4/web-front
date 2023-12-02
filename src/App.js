206ea64c5bdbd3d62e6bc50ae3e439bf4bd1579b
최수미 <choisumi@choesumiui-MacBookAir.local>
2023-12-02 23:20:53 +0900

    FIX: package.json conflict error

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Level2 from './component/level2Description';
 import Level3 from './component/level3Description';
 import Login from './component/loginPage';
 import WearVR from './component/wearVR';
+import Title from './component/title';
 
 // <Route path="" element={<d/>} /> <-- 이건 혹시 뭘까요..?
 
@@ -22,6 +23,7 @@ function App() {
           <Route path="/level/3" element={<Level3 />} />
           
           <Route path="/wearVR" element={<WearVR />} />
+          <Route path="/title" element={<Title />} />
         </Routes>
       </BrowserRouter>
     </div>
